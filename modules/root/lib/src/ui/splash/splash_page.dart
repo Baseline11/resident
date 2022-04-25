5bb5cbf545ada2c18ce16c3910106408647fde4a
Mohsin <mohsin2596@gmail.com>
2022-04-25 19:40:07 +0500

    Added local storage

--- modules/root/lib/src/ui/splash/splash_page.dart
+++ modules/root/lib/src/ui/splash/splash_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:root/components/assets/assets.gen.dart';
 
+import '../../../riverflow/bootstrap/signal/bootstrap_signal.dart';
+
 class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
@@ -14,7 +16,7 @@ class _SplashPageState extends ConsumerState<SplashPage> {
   void initState() {
     super.initState();
     WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
-      //ref.watch(bootstrapSignalProvider).dispatch();
+      ref.watch(bootstrapSignalProvider).dispatch();
     });
   }
 
