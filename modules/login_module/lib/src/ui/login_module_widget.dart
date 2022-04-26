bdc128eb6334a8610e94d069dfd6c10a9ade5c5f
Mohsin <mohsin2596@gmail.com>
2022-04-27 04:21:57 +0500

    Login module with navigator refactored

--- modules/login_module/lib/src/ui/login_module_widget.dart
+++ modules/login_module/lib/src/ui/login_module_widget.dart
@@ -1,49 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:login_module/riverflow/config/config.dart';
+import 'package:login_module/src/ui/pages/login_page.dart';
 import 'package:riverflow/widgets/module_widget.dart';
-import 'package:root/riverflow/user/observable/user_observable.dart';
-
-import '../../riverflow/payload/login_payload.dart';
-import '../../riverflow/signal/login_signal.dart';
 
 class LoginModule extends StatelessWidget {
   const LoginModule({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      pages: [
-        MaterialPage(
-          child: ModuleWidget(
-              configure: () =>
-                  configureLoginModule(ProviderScope.containerOf(context)),
-              builder: (_) => Scaffold(
-                    body: SafeArea(
-                      child: Column(
-                        children: [
-                          Consumer(
-                              builder: (context, ref, _) => Text(
-                                  'Login module ${ref.watch(userObservableProvider).isLoggedIn}')),
-                          Consumer(
-                            builder: (context, ref, _) => MaterialButton(
-                              onPressed: () {
-                                ref.watch(loginUserSignalProvider).dispatch(
-                                    LoginUserPayload(
-                                        email: "email", password: "password"));
-                              },
-                              child: const Text('Login'),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  )),
-        ),
-      ],
-      onPopPage: (route, result) {
-        return route.didPop(result);
-      },
-    );
+    return ModuleWidget(
+        configure: () =>
+            configureLoginModule(ProviderScope.containerOf(context)),
+        builder: (_) => Navigator(
+              pages: [
+                MaterialPage(child: LoginPage()),
+              ],
+              onPopPage: (route, result) {
+                return route.didPop(result);
+              },
+            ));
   }
 }
