d0a7f09dbe834c43631a922a14ce8b2e5d30679c
Mohsin <mohsin2596@gmail.com>
2022-04-26 13:44:24 +0500

    Onboarding module

---
+++ modules/onboarding_module/lib/components/assets/assets.gen.dart
@@ -0,0 +1,71 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class Assets {
+  Assets._();
+
+  static const AssetGenImage onboarding1 =
+      AssetGenImage('assets/onboarding_1.png');
+  static const AssetGenImage onboarding2 =
+      AssetGenImage('assets/onboarding_2.png');
+  static const AssetGenImage onboarding3 =
+      AssetGenImage('assets/onboarding_3.png');
+  static const AssetGenImage onboarding4 =
+      AssetGenImage('assets/onboarding_4.png');
+}
+
+class AssetGenImage extends AssetImage {
+  const AssetGenImage(String assetName)
+      : super(assetName, package: 'onboarding_module');
+
+  Image image({
+    Key? key,
+    ImageFrameBuilder? frameBuilder,
+    ImageLoadingBuilder? loadingBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? width,
+    double? height,
+    Color? color,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    FilterQuality filterQuality = FilterQuality.low,
+  }) {
+    return Image(
+      key: key,
+      image: this,
+      frameBuilder: frameBuilder,
+      loadingBuilder: loadingBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      filterQuality: filterQuality,
+    );
+  }
+
+  String get path => assetName;
+}
---
+++ modules/onboarding_module/lib/src/onboarding_module_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingModule extends StatelessWidget {
+  const OnboardingModule({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
