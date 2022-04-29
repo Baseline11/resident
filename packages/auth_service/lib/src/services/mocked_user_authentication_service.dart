005782268f8fb298286b076037c78b4d854a2714
Matheus Moraes <matheus7mm@hotmail.com>
2022-04-28 23:32:21 -0300

    MockedUserAuthService refactor

--- packages/auth_service/lib/src/services/mocked_user_authentication_service.dart
+++ packages/auth_service/lib/src/services/mocked_user_authentication_service.dart
@@ -3,23 +3,37 @@ import 'package:auth_service/src/domain/entities/user_auth_entity.dart';
 import 'package:riverpod/src/framework.dart';
 
 class MockedUserAuthService extends UserAuthenticationService {
-  MockedUserAuthService(ProviderContainer providerContainer)
-      : super(providerContainer);
+  final Future<UserAuthEntity> Function()? emailLoginFunction;
+  final Future<UserAuthEntity> Function()? verifyCodeFunction;
+  final Future<bool> Function()? verifyPhoneNumberFunction;
+
+  MockedUserAuthService(
+    ProviderContainer providerContainer, {
+    this.emailLoginFunction,
+    this.verifyCodeFunction,
+    this.verifyPhoneNumberFunction,
+  }) : super(providerContainer);
 
   @override
   Future<UserAuthEntity> emailLogin(
       {required AuthenticationParams params}) async {
-    return UserAuthEntity(token: "token");
+    return emailLoginFunction != null
+        ? await emailLoginFunction!()
+        : const UserAuthEntity(token: "token");
   }
 
   @override
   Future<UserAuthEntity> verifyCode({required String code}) async {
-    return UserAuthEntity(token: "token");
+    return verifyCodeFunction != null
+        ? await verifyCodeFunction!()
+        : const UserAuthEntity(token: "token");
   }
 
   @override
   Future<bool> verifyPhoneNumber(
       {required String phoneNumber, bool forceResend = false}) async {
-    return true;
+    return verifyPhoneNumberFunction != null
+        ? await verifyPhoneNumberFunction!()
+        : true;
   }
 }
